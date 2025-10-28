44c72752b207bcf5da0c9e2b16ccf28ec3df7fc0
ManihaHashmi2511 <it.solution8800@gmail.com>
2025-10-28 19:00:30 +0500

    serverless http added

--- api/index.js
+++ api/index.js
@@ -3,17 +3,17 @@ const cors = require('cors');
 
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
-const userRouter = require('./App/Routers/User.Routes');
-const companyRouter = require('./App/Routers/Company.Routes');
-const jobRouter = require('./App/Routers/Job.Routes');
-const applicationRouter = require('./App/Routers/Application.Routes');
-const path = require('path');
+const userRouter = require('../App/Routers/User.Routes');
+const companyRouter = require('../App/Routers/Company.Routes');
+const jobRouter = require('../App/Routers/Job.Routes');
+const applicationRouter = require('../App/Routers/Application.Routes');
+const serverLess = require('serverless-http');
 require('dotenv').config();
 
 
 const app = express();
 
-const _dirname = path.resolve();
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // to parse URL-encoded bodies
@@ -33,23 +33,11 @@ app.use('/api/companies', companyRouter); //http://localhost:8000/api/companies
 app.use('/api/jobs', jobRouter); //http://localhost:8000/api/jobs
 app.use('/api/applications', applicationRouter); //http://localhost:8000/api/applications
 
-app.use(express.static(path.join(_dirname, '/Frontend/dist')));
-app.use(/.*/, (req, res) => {
-  res.sendFile(path.resolve(_dirname, 'Frontend', 'dist', 'index.html'));
-});
-
-
-
-
-
-const PORT = process.env.PORT || 8080;
 
 mongoose.connect(process.env.DB_URL)
   .then(() => {
     console.log('Connected to MongoDB');
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
+    
   })
   .catch((err) => {
     console.error('MongoDB connection error:', err.message);
@@ -60,3 +48,6 @@ app.use((err, req, res, next) => {
   console.error('Unhandled error:', err);
   res.status(500).json({ message: 'Internal server error' });
 });
+
+module.exports = app;
+module.exports.handler = serverLess(app);
